0fc3e6afec45aeb7d5ebd92b6d997d52ee218228
schuemie <schuemie@ohdsi.org>
2018-11-27 17:32:55 -0500

    Computing metrics only on estimable controls

--- MethodEvalViewer/server.R
+++ MethodEvalViewer/server.R
@@ -69,6 +69,8 @@ shinyServer(function(input, output, session) {
     if (input$trueRr == "Overall") {
       computeMetrics <- function(i) {
         forEval <- subset[subset$method == combis$method[i] & subset$analysisId == combis$analysisId[i], ]
+        nonEstimable <- round(mean(forEval$seLogRr >= 99), 2)
+        forEval <- forEval[forEval$seLogRr < 99, ]
         roc <- pROC::roc(forEval$targetEffectSize > 1, forEval$logRr, algorithm = 3)
         auc <- round(pROC::auc(roc), 2)
         mse <- round(mean((forEval$logRr - log(forEval$trueEffectSize))^2), 2)
@@ -76,7 +78,6 @@ shinyServer(function(input, output, session) {
         meanP <- round(exp(mean(log(1/(forEval$seLogRr^2)))), 2)
         type1 <- round(mean(forEval$p[forEval$targetEffectSize == 1] < 0.05), 2)
         type2 <- round(mean(forEval$p[forEval$targetEffectSize > 1] >= 0.05), 2)
-        nonEstimable <- round(mean(forEval$seLogRr == 999), 2)
         return(c(auc = auc, coverage = coverage, meanP = meanP, mse = mse, type1 = type1, type2 = type2, nonEstimable = nonEstimable))
       }
       combis <- cbind(combis, as.data.frame(t(sapply(1:nrow(combis), computeMetrics))))
@@ -84,6 +85,8 @@ shinyServer(function(input, output, session) {
       # trueRr <- input$trueRr
       computeMetrics <- function(i) {
         forEval <- subset[subset$method == combis$method[i] & subset$analysisId == combis$analysisId[i] & subset$targetEffectSize == input$trueRr, ]
+        nonEstimable <- round(mean(forEval$seLogRr >= 99), 2)
+        forEval <- forEval[forEval$seLogRr < 99, ]
         mse <- round(mean((forEval$logRr - log(forEval$trueEffectSize))^2), 2)
         coverage <- round(mean(forEval$ci95Lb < forEval$trueEffectSize & forEval$ci95Ub > forEval$trueEffectSize), 2)
         meanP <- round(exp(mean(log(1/(forEval$seLogRr^2)))), 2)
@@ -91,14 +94,12 @@ shinyServer(function(input, output, session) {
           auc <- NA
           type1 <- round(mean(forEval$p < 0.05), 2)  
           type2 <- NA
-          nonEstimable <- round(mean(forEval$seLogRr == 999), 2)
         } else {
           negAndPos <- subset[subset$method == combis$method[i] & subset$analysisId == combis$analysisId[i] & (subset$targetEffectSize == input$trueRr | subset$targetEffectSize == 1), ]
           roc <- pROC::roc(negAndPos$targetEffectSize > 1, negAndPos$logRr, algorithm = 3)
           auc <- round(pROC::auc(roc), 2)
           type1 <- NA
           type2 <- round(mean(forEval$p[forEval$targetEffectSize > 1] >= 0.05), 2)  
-          nonEstimable <- round(mean(forEval$seLogRr == 999), 2)
         }
         return(c(auc = auc, coverage = coverage, meanP = meanP, mse = mse, type1 = type1, type2 = type2, nonEstimable = nonEstimable))
       }
