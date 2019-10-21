04563f923879b882b34398129919b193cfc2c22a
Marc Suchard <msuchard@ucla.edu>
2019-10-21 14:07:44 -0700

    trial and error

--- LegendBasicViewer/ui.R
+++ LegendBasicViewer/ui.R
@@ -27,8 +27,9 @@ shinyUI(
             tabsetPanel(id = "mainTabsetPanel",
                         tabPanel("About",
                                  p("The Observational Health Data Sciences and Informatics (OHDSI) international collaborative has developed the Large-Scale Evidence Generation and Evaluation across a Network of Databases (LEGEND) research initiative. LEGEND strives to produce reproducible evidence based on observational data and to fill in evidence gaps in medicine",
-                                   style = "word-wrap: break-word; white-space: normal; width: 100px"),
-                                 p("This app provides access to all LEGEND study results. To start, click on the 'Specific researc questions' tab.")
+                                   style = "word-wrap: break-word; white-space: normal; width: 500px"),
+                                 p("This app provides access to all LEGEND study results. To start, click on the 'Specific researc questions' tab.",
+                                   style = "word-wrap: break-word; white-space: normal; width: 500px")
                         ),
                         tabPanel("Specific research questions",
                                  fluidRow(
