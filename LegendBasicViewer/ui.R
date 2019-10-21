9aeebd80036a3626c1571836257bc9365d2b4de9
Marc Suchard <msuchard@ucla.edu>
2019-10-21 14:14:52 -0700

    barely acceptable, but better than -do not use- previously.

--- LegendBasicViewer/ui.R
+++ LegendBasicViewer/ui.R
@@ -27,9 +27,9 @@ shinyUI(
             tabsetPanel(id = "mainTabsetPanel",
                         tabPanel("About",
                                  p("The Observational Health Data Sciences and Informatics (OHDSI) international collaborative has developed the Large-Scale Evidence Generation and Evaluation across a Network of Databases (LEGEND) research initiative. LEGEND strives to produce reproducible evidence based on observational data and to fill in evidence gaps in medicine.",
-                                   style = "word-wrap: break-word; white-space: normal; width: 650px"),
-                                 p("This app provides access to all LEGEND study results. To start, click on the 'Specific researc questions' tab.",
-                                   style = "word-wrap: break-word; white-space: normal; width: 650px")
+                                   style = "word-wrap: break-word; white-space: normal; width: 640px"),
+                                 p("This app provides access to all LEGEND study results. To start, click on the 'Specific research questions' tab.",
+                                   style = "word-wrap: break-word; white-space: normal; width: 640px")
                         ),
                         tabPanel("Specific research questions",
                                  fluidRow(
