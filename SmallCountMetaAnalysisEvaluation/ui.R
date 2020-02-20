ac037a03248c1612b9aa93dcccad3ebad38d6db5
schuemie <schuemie@ohdsi.org>
2020-02-20 04:19:14 -0500

    Adding violin plots to small-counts meta-analysis results

--- SmallCountMetaAnalysisEvaluation/ui.R
+++ SmallCountMetaAnalysisEvaluation/ui.R
@@ -15,12 +15,18 @@ shinyUI(
                                    column(3,
                                           checkboxGroupInput("typeFixed", "Meta-analysis algorithm", choices = typesFixed, selected = typesFixed),
                                           lapply(simParamsFixed, createSimParamWidget, results = resultsFixed, suffix = "Fixed"),
-                                          checkboxGroupInput("metricFixed", "Metric", choices = metricsFixed, selected = metricsFixed),
-                                   ), 
+                                          checkboxGroupInput("metricFixed", "Metric", choices = metricsFixed, selected = metricsFixed)
+                                          ),  
                                    column(9,
                                           tabsetPanel(id = "fixedEffectsTabsetPanel",
                                                       type = "pills",
-                                                      tabPanel("Scatter plots",
+                                                      tabPanel("Violin plots",
+                                                               plotOutput("mainViolinPlotFixed", height = 800),
+                                                               div(align = "center",
+                                                                   radioButtons("simParamFixedRadioButton", "", choices = simParamsFixed, selected = simParamsFixed[1], inline = TRUE)
+                                                               ),
+                                                               uiOutput("mainViolinCaptionFixed")
+                                                      ),tabPanel("Scatter plots",
                                                                uiOutput("hoverInfoPlotFixed"),
                                                                plotOutput("mainPlotFixed", height = 800, hover = hoverOpts("plotHoverMainPlotFixed", delay = 100, delayType = "debounce")),
                                                                uiOutput("mainCaptionFixed")
@@ -38,11 +44,18 @@ shinyUI(
                                    column(3,
                                           checkboxGroupInput("typeRandom", "Meta-analysis algorithm", choices = typesRandom, selected = typesRandom[grepl("random", typesRandom)]),
                                           lapply(simParamsRandom, createSimParamWidget, results = resultsRandom, suffix = "Random"),
-                                          checkboxGroupInput("metricRandom", "Metric", choices = metricsRandom, selected = metricsRandom[!grepl("Tau", metricsRandom)]),
+                                          checkboxGroupInput("metricRandom", "Metric", choices = metricsRandom, selected = metricsRandom[!grepl("Tau", metricsRandom)])
                                    ), 
                                    column(9,
-                                          tabsetPanel(id = "fixedEffectsTabsetPanel",
+                                          tabsetPanel(id = "randomEffectsTabsetPanel",
                                                       type = "pills",
+                                                      tabPanel("Violin plots",
+                                                               plotOutput("mainViolinPlotRandom", height = 800),
+                                                               div(align = "center",
+                                                                   radioButtons("simParamRandomRadioButton", "", choices = simParamsRandom, selected = simParamsRandom[1], inline = TRUE)
+                                                               ),
+                                                               uiOutput("mainViolinCaptionRandom")
+                                                               ),
                                                       tabPanel("Scatter plots",
                                                                uiOutput("hoverInfoPlotRandom"),
                                                                plotOutput("mainPlotRandom", height = 800, hover = hoverOpts("plotHoverMainPlotRandom", delay = 100, delayType = "debounce")),
