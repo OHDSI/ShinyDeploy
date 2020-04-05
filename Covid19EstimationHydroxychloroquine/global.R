237cfe91dedf5b56818167a9b0d6df9ec74e8dbf
Jamie Weaver <jweave17@its.jnj.com>
2020-04-05 12:32:47 -0400

    updated landing page

--- Covid19EstimationHydroxychloroquine/global.R
+++ Covid19EstimationHydroxychloroquine/global.R
@@ -1,7 +1,7 @@
 source("DataPulls.R")
 source("PlotsAndTables.R")
 
-# shinySettings <- list(dataFolder = "G:/StudyResults/Covid19EstimationHydroxychloroquine_2/SIDIAP/shinyData", blind = TRUE)
+#shinySettings <- list(dataFolder = "G:/StudyResults/Covid19EstimationHydroxychloroquine_2/IPCI/shinyData", blind = TRUE)
 shinySettings <- list(dataFolder = "./data", blind = FALSE)
 
 dataFolder <- shinySettings$dataFolder
