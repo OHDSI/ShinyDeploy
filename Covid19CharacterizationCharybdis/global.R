5b4e2ff5226d1c7d3367389844db8541a2cb2d33
Anthony Sena <asena5@its.jnj.com>
2020-07-26 07:58:52 -0400

    Add VA diagnostics

--- Covid19CharacterizationCharybdis/global.R
+++ Covid19CharacterizationCharybdis/global.R
@@ -12,7 +12,7 @@ if (!exists("shinySettings")) {
     shinySettings <- list(storage = "filesystem", dataFolder = "data", dataFile = "PreMerged.RData")
   } else if (is_installed("aws.s3") && is_installed("aws.ec2metadata")){
     library("aws.ec2metadata")
-    shinySettings <- list(storage = "s3", dataFolder = Sys.getenv("OHDSI_SHINY_DATA_BUCKET"), dataFile = "Covid19CharacterizationCharybdis/gq4h1nyb_PreMerged.RData")
+    shinySettings <- list(storage = "s3", dataFolder = Sys.getenv("OHDSI_SHINY_DATA_BUCKET"), dataFile = "Covid19CharacterizationCharybdis/5mk21tge_PreMerged.RData")
   } else {
     shinySettings <- list(storage = "filesystem", dataFolder = "c:/temp/exampleStudy", dataFile = "PreMerged.RData")
   }
