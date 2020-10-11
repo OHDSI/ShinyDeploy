cb78c3c709db06f815de5fe2bbecb4c8f61876f4
Anna Ostropolets <anna.ostropolets@odysseusinc.com>
2020-10-10 21:54:45 -0400

    adding recommendations for CS

--- PHOEBE/server.R
+++ PHOEBE/server.R
@@ -2,27 +2,93 @@ library(shiny)
 library(DT)
 source("DataPulls.R")
 
+renderBorderTag <-  function() {
+  return(htmltools::withTags(
+    div(class="cohort-heading")
+  ))
+}
+
 shinyServer(function(input, output) {
   
-  # use user's input
-  data <- eventReactive(input$inputAction, {
+  # use user's input for initial concept
+  InitialConceptData <- eventReactive(input$inputActionConcept, {
     if (input$sourceString == '') {
       stop("need to input a search string")
     }  
     else {  
-      initial_concept <- loadDataFromDB(connPool, input$sourceDomain, input$sourceString)
+      initial_concept <- loadConceptFromDB(connPool, input$sourceDomain, input$sourceString)
       return(initial_concept)
     }  
   })
   
-  # output the result of the function into a table
-  output$table <- DT::renderDataTable({
-    dt <- data()
+  # use user's input for initial concept
+  ConceptSetStandardData <- eventReactive(input$inputActionList, {
+    if (input$conceptList == '') {
+      stop("need to input a concept list")
+    }  
+    else {  
+      concept_list <- loadRecommenderStandardFromDB(connPool, input$conceptList)
+      return(concept_list)
+    }  
+  })
+  
+  ConceptSetSourceData <- eventReactive(input$inputActionList, {
+    if (input$conceptList == '') {
+      stop("need to input a concept list")
+    }  
+    else {  
+      concept_list <- loadRecommenderSourceFromDB(connPool, input$conceptList)
+      return(concept_list)
+    }  
+  })
+  
+  # output the initial concept recommendation into a table
+  output$InitialConceptTable <- DT::renderDataTable({
+    dt <- InitialConceptData()
     colnames(dt) <- c("concept id","concept name","vocabulary id","domain id",
                       "standard concept","record count","database count", "record with descendants count",
                       "database with descendants count")
     dt
+  })
+  
+  # output the initial concept recommendation into a table
+  output$ConceptSetStandardTable <- DT::renderDataTable({
+    dt <- ConceptSetStandardData()
+    colnames(dt) <- c("concept id","concept name","vocabulary id","domain id",
+                      "standard concept","concept_in_set","record count","database count", "record with descendants count",
+                      "database with descendants count")
+    
+    dt
   },
-  options = list(autoWidth = TRUE, rownames = FALSE)
-  )
+  filter = 'top')
+  
+  # output the initial concept recommendation into a table
+  output$ConceptSetSourceTable <- DT::renderDataTable({
+    dt <- ConceptSetSourceData()
+    colnames(dt) <- c("concept id","concept name","domain id","vocabulary id","standard concept","record count","database count",
+                      "record with descendants count", "database with descendants count", "source concept id", "source concept name",
+                      "source record count","source database count")
+    dt
+  },
+  filter = 'top',
+  options = list(scrollX = TRUE))
+  
+  output$dlConceptSetStandard <- downloadHandler(
+    filename = function() {
+      'concept_set_standard.csv'
+      },
+    content = function(file) {
+    table<-ConceptSetStandardData()
+    write.csv(table, file, row.names = FALSE, na = "")
+  })
+  
+  output$borderInitialConcept <- renderUI({
+    return(renderBorderTag())
+  })
+  
+  output$borderConceptSet <- renderUI({
+    return(renderBorderTag())
+  })
+  
+  
 })
