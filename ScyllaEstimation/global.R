d673f7ea44c12ec36a8eec75b632c0cc9c016331
schuemie <schuemie@ohdsi.org>
2020-11-17 09:08:11 -0500

    Adding study diagnostics for Scylla

---
+++ ScyllaEstimation/global.R
@@ -0,0 +1,48 @@
+source("DataPulls.R")
+source("PlotsAndTables.R")
+
+shinySettings <- list(dataFolder = "data", blind = TRUE)
+dataFolder <- shinySettings$dataFolder
+blind <- shinySettings$blind
+connection <- NULL
+positiveControlOutcome <- NULL
+
+splittableTables <- c("covariate_balance", "preference_score_dist", "kaplan_meier_dist")
+
+files <- list.files(dataFolder, pattern = ".rds")
+
+# Find part to remove from all file names (usually databaseId):
+databaseFileName <- files[grepl("^database", files)]
+removeParts <- paste0(gsub("database", "", databaseFileName), "$")
+
+# Remove data already in global environment:
+for (removePart in removeParts) {
+  tableNames <- gsub("_t[0-9]+_c[0-9]+$", "", gsub(removePart, "", files[grepl(removePart, files)]))
+  camelCaseNames <- SqlRender::snakeCaseToCamelCase(tableNames)
+  camelCaseNames <- unique(camelCaseNames)
+  camelCaseNames <- camelCaseNames[!(camelCaseNames %in% SqlRender::snakeCaseToCamelCase(splittableTables))]
+  suppressWarnings(rm(list = camelCaseNames))
+}
+
+# Load data from data folder. R data objects will get names derived from the filename:
+loadFile <- function(file, removePart) {
+  tableName <- gsub("_t[0-9]+_c[0-9]+$", "", gsub(removePart, "", file))
+  camelCaseName <- SqlRender::snakeCaseToCamelCase(tableName)
+  if (!(tableName %in% splittableTables)) {
+    newData <- readRDS(file.path(dataFolder, file))
+    colnames(newData) <- SqlRender::snakeCaseToCamelCase(colnames(newData))
+    if (exists(camelCaseName, envir = .GlobalEnv)) {
+      existingData <- get(camelCaseName, envir = .GlobalEnv)
+      newData <- rbind(existingData, newData)
+      newData <- unique(newData)
+    }
+    assign(camelCaseName, newData, envir = .GlobalEnv)
+  }
+  invisible(NULL)
+}
+for (removePart in removeParts) {
+  lapply(files[grepl(removePart, files)], loadFile, removePart)
+}
+
+tcos <- unique(cohortMethodResult[, c("targetId", "comparatorId", "outcomeId")])
+tcos <- tcos[tcos$outcomeId %in% outcomeOfInterest$outcomeId, ]
