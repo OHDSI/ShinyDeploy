2a50a57d8f9fccc37be9593351f4b2ba9a73cc71
keesvanbochove <kees@thehyve.nl>
2021-08-31 11:53:09 +0200

    Add helper function for installed packages to PioneerWatchfulWaitingDiag

--- PioneerWatchfulWaitingDiag/global.R
+++ PioneerWatchfulWaitingDiag/global.R
@@ -101,6 +101,13 @@ suppressWarnings(rm(
   list = SqlRender::snakeCaseToCamelCase(dataModelSpecifications$tableName)
 ))
 
+# Borrowed from devtools:
+# https://github.com/hadley/devtools/blob/ba7a5a4abd8258c52cb156e7b26bb4bf47a79f0b/R/utils.r#L44
+is_installed <- function(pkg, version = 0) {
+  installed_version <- tryCatch(utils::packageVersion(pkg), error = function(e) NA)
+  !is.na(installed_version) && installed_version >= version
+}
+
 if (databaseMode) {
   onStop(function() {
     if (DBI::dbIsValid(connectionPool)) {
