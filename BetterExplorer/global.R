bf5c60632012b06e6d56acac611450d1d844935b
fanbu1995 <fb75@duke.edu>
2023-04-25 14:23:39 -0700

    clean up global.R script to use one data schema

--- BetterExplorer/global.R
+++ BetterExplorer/global.R
@@ -1,48 +1,18 @@
 source("dataPulls.R")
 
-connectionPool <- pool::dbPool(drv = DatabaseConnector::DatabaseConnectorDriver(),
-                               dbms = "postgresql",
-                               server = paste(Sys.getenv("shinydbServer"),
-                                              Sys.getenv("shinydbDatabase"),
-                                              sep = "/"),
-                               port = Sys.getenv("shinydbPort"),
-                               # user = Sys.getenv("shinydbUser"),
-                               # password = Sys.getenv("shinydbPw"))
-                               user = Sys.getenv("eumaeusdbUser"),
-                               password = Sys.getenv("eumaeusdbPw"))
+# set up BETTER connections..... 
 
-# OLD: credentials used for local testing...
-# connectionPool <- pool::dbPool(drv = DatabaseConnector::DatabaseConnectorDriver(),
-#                                dbms = "postgresql",
-#                                server = paste(keyring::key_get("eumaeusServer"),
-#                                               keyring::key_get("eumaeusDatabase"),
-#                                               sep = "/"),
-#                                user = keyring::key_get("eumaeusUser"),
-#                                password = keyring::key_get("eumaeusPassword"))
-
-
-# onStop(function() {
-#   if (DBI::dbIsValid(connectionPool)) {
-#     writeLines("Closing connection pool")
-#     pool::poolClose(connectionPool)
-#   }
-# })
-
-schema <- "eumaeus"
-
-analysis <- loadEntireTable(connectionPool, schema, "analysis")
-database <- loadEntireTable(connectionPool, schema, "database")
-exposure <- loadEntireTable(connectionPool, schema, "exposure")
-negativeControlOutcome <- loadEntireTable(connectionPool, schema, "negative_control_outcome")
-positiveControlOutcome <- loadEntireTable(connectionPool, schema, "positive_control_outcome")
-
-trueRrs <- c("Any", 1, unique(positiveControlOutcome$effectSize))
-
-
-# BETTER connections..... 
-
-# try to use the Eumaeus app user
-connectionPoolBetter <- connectionPool
+# use the Eumaeus app user credentials
+connectionPoolBetter <- pool::dbPool(drv = DatabaseConnector::DatabaseConnectorDriver(),
+                                     dbms = "postgresql",
+                                     server = paste(Sys.getenv("shinydbServer"),
+                                                    Sys.getenv("shinydbDatabase"),
+                                                    sep = "/"),
+                                     port = Sys.getenv("shinydbPort"),
+                                     # user = Sys.getenv("shinydbUser"),
+                                     # password = Sys.getenv("shinydbPw"))
+                                     user = Sys.getenv("eumaeusdbUser"),
+                                     password = Sys.getenv("eumaeusdbPw"))
 
 ## credentials for local testing
 # connectionPoolBetter <- pool::dbPool(drv = DatabaseConnector::DatabaseConnectorDriver(),
@@ -53,32 +23,30 @@ connectionPoolBetter <- connectionPool
 #                                user = keyring::key_get("betterUser"),
 #                                password = keyring::key_get("betterPassword"))
 
-## try to use master shinydb user credentials instead......
-# connectionPoolBetter <- pool::dbPool(drv = DatabaseConnector::DatabaseConnectorDriver(),
-#                                dbms = "postgresql",
-#                                server = paste(Sys.getenv("shinydbServer"),
-#                                               Sys.getenv("shinydbDatabase"),
-#                                               sep = "/"),
-#                                port = Sys.getenv("shinydbPort"),
-#                                user = Sys.getenv("shinydbUser"),
-#                                password = Sys.getenv("shinydbPw"))
-# 
-# 
-onStop(function() {
-  if (DBI::dbIsValid(connectionPoolBetter)) {
-    writeLines("Closing connection pool")
-    pool::poolClose(connectionPoolBetter)
-  }
-})
-
 schema <- "better_results"
 
+analysis <- loadEntireTable(connectionPoolBetter, schema, "analysis")
+database <- loadEntireTable(connectionPoolBetter, schema, "database")
+exposure <- loadEntireTable(connectionPoolBetter, schema, "exposure")
+positiveControlOutcome <- loadEntireTable(connectionPoolBetter, schema, "all_ipcs")
+
+trueRrs <- c("Any", 1, unique(positiveControlOutcome$effectSize))
+
 mses = loadEntireTable(connectionPoolBetter, schema, "mses")
-priors = loadEntireTable(connectionPoolBetter, schema, "priors")
+#priors = loadEntireTable(connectionPoolBetter, schema, "priors")
 #type1s = loadEntireTable(connectionPoolBetter, schema, "type1s")
 #tts = loadEntireTable(connectionPoolBetter, schema, "time_to_signal")
 
 ## filter analysis to exclude "filtered" Historical Comparator results
 analysis = analysis %>% filter(!(method == 'HistoricalComparator' & analysisId >= 13))
 sensitivity_levels = c(.25, 0.5)
+
+# close pool when session ends
+onStop(function() {
+  if (DBI::dbIsValid(connectionPoolBetter)) {
+    writeLines("Closing connection pool")
+    pool::poolClose(connectionPoolBetter)
+    #pool::poolClose(connectionPool)
+  }
+})
 
