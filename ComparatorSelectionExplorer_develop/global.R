2a559be86a7b97d285e014434648f8acd5f22b44
Jamie Gilbert <JGilber2@its.jnj.com>
2023-06-26 13:45:23 -0700

    Update global.R

--- ComparatorSelectionExplorer_develop/global.R
+++ ComparatorSelectionExplorer_develop/global.R
@@ -1,30 +1,15 @@
-# details of results database
-# Created for data.ohdsi.org
-# details of results database
 
-JDBC_PATH <- Sys.getenv("DATABASECONNECTOR_JAR_FOLDER", "./.drivers")
-
-if (!dir.exists(JDBC_PATH)) {
-  dir.create(JDBC_PATH)
-}
 
 connectionDetails <- DatabaseConnector::createConnectionDetails(
   dbms = "postgresql",
-  user = Sys.getenv("REWARD_DB_USER"),
-  password = Sys.getenv("REWARD_DB_PASSWORD"),
-  server = "reward.cterqq54xyuu.us-east-1.rds.amazonaws.com/rewardb_dev",
-  port = 5432,
-  pathToDriver = JDBC_PATH
+  user = Sys.getenv("phenotypeLibrarydbUser"),
+  password = Sys.getenv("phenotypeLibrarydbPw"),
+  server = paste0(Sys.getenv("phenotypeLibraryServer"), "/", Sys.getenv("phenotypeLibrarydb")),
+  port = 5432
 )
 
-
-if (connectionDetails$dbms != "sqlite" &&
-  !any(grepl(connectionDetails$dbms, list.files(JDBC_PATH)))) {
-  DatabaseConnector::downloadJdbcDrivers(connectionDetails$dbms)
-}
-
-resultsSchema <- "cse_062023"
-tablePrefix <- "cse_"
+tablePrefix <- "cse062023_"
+resultsSchema <- "comparator_selector"
 
 # decimal formatters
 fmtSim <- "%.3f"
