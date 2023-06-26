fff033bf39a18d90b7f59d540c155295760d642b
Jamie Gilbert <JGilber2@its.jnj.com>
2023-06-26 13:41:08 -0700

    Add files via upload
    Updates to app

--- ComparatorSelectionExplorer_develop/global.R
+++ ComparatorSelectionExplorer_develop/global.R
@@ -1,13 +1,31 @@
 # details of results database
 # Created for data.ohdsi.org
 # details of results database
+
+JDBC_PATH <- Sys.getenv("DATABASECONNECTOR_JAR_FOLDER", "./.drivers")
+
+if (!dir.exists(JDBC_PATH)) {
+  dir.create(JDBC_PATH)
+}
+
 connectionDetails <- DatabaseConnector::createConnectionDetails(
   dbms = "postgresql",
-  user = Sys.getenv("phenotypeLibrarydbUser"),
-  password = Sys.getenv("phenotypeLibrarydbPw"),
-  server = paste0(Sys.getenv("phenotypeLibraryServer"), "/", Sys.getenv("phenotypeLibrarydb")),
-  port = 5432
+  user = Sys.getenv("REWARD_DB_USER"),
+  password = Sys.getenv("REWARD_DB_PASSWORD"),
+  server = "reward.cterqq54xyuu.us-east-1.rds.amazonaws.com/rewardb_dev",
+  port = 5432,
+  pathToDriver = JDBC_PATH
 )
 
-tablePrefix <- "cse0223_"
-resultsSchema <- "comparator_selector"
+
+if (connectionDetails$dbms != "sqlite" &&
+  !any(grepl(connectionDetails$dbms, list.files(JDBC_PATH)))) {
+  DatabaseConnector::downloadJdbcDrivers(connectionDetails$dbms)
+}
+
+resultsSchema <- "cse_062023"
+tablePrefix <- "cse_"
+
+# decimal formatters
+fmtSim <- "%.3f"
+fmtSmd <- "%.2f"
