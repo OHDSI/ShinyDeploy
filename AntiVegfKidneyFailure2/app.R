b1baf388a3a4c129a3b9129ec4aeb2929a94624d
nhall6 <nhall6@its.jnj.com>
2023-08-17 10:43:49 -0400

    Trying to update app.R again to conform with versions of packages being used, replicating what was done in first AntiVegfKidneyFailure app

--- AntiVegfKidneyFailure2/app.R
+++ AntiVegfKidneyFailure2/app.R
@@ -8,33 +8,115 @@ schema <- Sys.getenv("antivegfkf2dbSchema")
 library(dplyr)
 library(ShinyAppBuilder)
 library(OhdsiShinyModules)
+
 config <- initializeModuleConfig() %>%
   addModuleConfig(
-    createDefaultAboutConfig()
+    createDefaultAboutConfig(
+      resultDatabaseDetails = list(
+      ),
+      useKeyring = F
+    )
   )  %>%
-  # addModuleConfig(
-  #   createDefaultDatasourcesConfig()
-  # )  %>%
   addModuleConfig(
-    createDefaultCohortGeneratorConfig()
+    createDefaultCohortGeneratorConfig(
+      resultDatabaseDetails = list(
+        tablePrefix = 'cg_',
+        schema = schema,
+        databaseTable = 'DATABASE_META_DATA'
+      ),
+      useKeyring = F
+    )
   ) %>%
   addModuleConfig(
-    createDefaultCohortDiagnosticsConfig()
+    createDefaultCohortDiagnosticsConfig(
+      resultDatabaseDetails = list(
+        tablePrefix = 'cd_',
+        #cohortTablePrefix = 'cg_',
+        schema = schema,
+        vocabularyDatabaseSchema = schema,
+        #databaseTable = 'DATABASE_META_DATA'
+        databaseTable = 'cd_DATABASE'
+      ),
+      useKeyring = F
+    )
   ) %>%
   addModuleConfig(
-    createDefaultCharacterizationConfig()
+    createDefaultCharacterizationConfig(
+      resultDatabaseDetails = list(
+        tablePrefix = 'c_',
+        cohortTablePrefix = 'cg_',
+        databaseTablePrefix = '',
+        schema = schema,
+        databaseTable = 'DATABASE_META_DATA',
+        incidenceTablePrefix = 'ci_'
+      ),
+      useKeyring = F
+    )
   ) %>%
   addModuleConfig(
-    createDefaultPredictionConfig()
+    createDefaultPredictionConfig(
+      resultDatabaseDetails = list(
+        tablePrefix = 'plp_',
+        cohortTablePrefix = 'cg_',
+        databaseTablePrefix = '',
+        schema = schema,
+        databaseTable = 'DATABASE_META_DATA'
+      ),
+      useKeyring = F
+    )
   ) %>%
   addModuleConfig(
-    createDefaultCohortMethodConfig()
+    createDefaultEstimationConfig(
+      resultDatabaseDetails = list(
+        tablePrefix = 'cm_',
+        cohortTablePrefix = 'cg_',
+        databaseTablePrefix = '',
+        schema = schema,
+        databaseTable = 'DATABASE_META_DATA'
+      ),
+      useKeyring = F
+    )
   ) %>%
   addModuleConfig(
-    createDefaultSccsConfig()
+    ShinyAppBuilder::createModuleConfig( 
+      moduleIcon = "people-arrows",
+      moduleId = 'sscs', 
+      tabName = 'SCCS',
+      shinyModulePackage = "OhdsiShinyModules",
+      moduleUiFunction = "sccsView",
+      moduleServerFunction = "sccsServer",
+      moduleDatabaseConnectionKeyUsername = 'sccs', 
+      moduleInfoBoxFile = "sccsHelperFile()",
+      resultDatabaseDetails = list(
+        tablePrefix = 'sccs_',
+        cohortTablePrefix = 'cg_',
+        databaseTablePrefix = '',
+        schema = schema,
+        databaseTable = 'DATABASE_META_DATA'
+      ),
+      useKeyring = F
+    )
   ) %>%
   addModuleConfig(
-    createDefaultEvidenceSynthesisConfig()
+    ShinyAppBuilder::createModuleConfig( 
+      moduleIcon = "object-group",#"meta",
+      moduleId = 'EvidenceSynthesis', 
+      tabName = 'Meta',
+      shinyModulePackage = "OhdsiShinyModules",
+      moduleUiFunction = "evidenceSynthesisViewer",
+      moduleServerFunction = "evidenceSynthesisServer",
+      moduleDatabaseConnectionKeyUsername = 'es', 
+      moduleInfoBoxFile = "evidenceSynthesisHelperFile()",
+      resultDatabaseDetails = list(
+        tablePrefix = 'es_',
+        cmTablePrefix = 'cm_',
+        cgTablePrefix = 'cg_',
+        sccsTablePrefix = 'sccs_',
+        schema = schema,
+        databaseMetaData = 'DATABASE_META_DATA'
+      ),
+      useKeyring = F
+    )
   )
 
 # Create connection details to results database ---------------
@@ -47,20 +129,10 @@ connectionDetails <- DatabaseConnector::createConnectionDetails(
 )
 
 # create a connection handler using the ResultModelManager package
-connection <- ResultModelManager::ConnectionHandler$new(connectionDetails)
-
-
-
-# create result schema settings
-resultDatabaseSettings <- createDefaultResultDatabaseSettings(
-  schema = schema
-)
+connection <- ResultModelManager::PooledConnectionHandler$new(connectionDetails)
+##connection <- ResultModelManager::ConnectionHandler$new(connectionDetails)
 
 # now create the shiny app based on the config file and view the results
 # based on the connection 
-##ShinyAppBuilder::createShinyApp(config = config, connection = connection)
-ShinyAppBuilder::viewShiny(
-  config = config, 
-  connection = connection, 
-  resultDatabaseSettings = resultDatabaseSettings
-)
+ShinyAppBuilder::createShinyApp(config = config, connection = connection)
+##ShinyAppBuilder::viewShiny(config = config, connection = connection)
